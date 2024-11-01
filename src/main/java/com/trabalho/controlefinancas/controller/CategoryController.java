ef45b4893390846deeea487bfc1b58864dbd3f3c
Diogo Valongo <divalongo@gmail.com>
2024-11-01 20:17:23 -0300

    fix: import

--- src/main/java/com/trabalho/controlefinancas/controller/CategoryController.java
+++ src/main/java/com/trabalho/controlefinancas/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.trabalho.controlefinancas.controller;
 
 import com.trabalho.controlefinancas.model.Category;
 import com.trabalho.controlefinancas.repository.CategoryRepository;
+import com.trabalho.controlefinancas.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Controller
@@ -31,9 +33,10 @@ public class CategoryController {
     @PostMapping("/add-category")
     public String addCategory(@RequestParam String name,
                               @RequestParam(required = false) String description,
+                              @RequestParam BigDecimal budget,
                               RedirectAttributes redirectAttributes) {
         try {
-            Category category = new Category(name, description);
+            Category category = new Category(name, description, budget);
             categoryService.addCategory(category);
             redirectAttributes.addFlashAttribute("message", "Category successfully added!");
             return "redirect:/categories";
